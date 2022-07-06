9af272fcafcdc515e9f23b4a5a5c97145c41c775
Bartholomé Gili <66025667+barthofu@users.noreply.github.com>
2022-07-06 09:37:43 +0000

    feat: DI support with tsyringe

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
+import 'reflect-metadata'
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
 
 export default MyApp
