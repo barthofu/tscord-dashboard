6207221b66617a20f3235db12a5f108b34793ff1
Mr-Artemus <31190188+Mr-Artemus@users.noreply.github.com>
2022-12-11 16:35:48 +0100

    Fix: Wrong protocol by `getAbsoluteUrl`

--- client/src/core/utils/functions/url.ts
+++ client/src/core/utils/functions/url.ts
@@ -1,12 +1,7 @@
-import { IncomingMessage } from "http"
-import absoluteUrl from "next-absolute-url"
-
-export const getAbsoluteUrl = (url: string, req?: IncomingMessage) => {
+export const getAbsoluteUrl = (url: string) => {
 
     if (url.startsWith('http')) return url
 
-    const host = req ? absoluteUrl(req).origin : absoluteUrl().origin
-
-    if (url.startsWith('/')) return `${host}${url}`
-    else return `${process.env['BASE_URL']}/${url}`
+    if(typeof window === 'undefined') return `${process.env['BASE_URL']}/${url}`
+    else return `${window.location.origin}/${url}`
 }
