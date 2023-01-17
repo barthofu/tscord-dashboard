9ef00dc0c739fc174109bfd298a36f1d01a46b83
barthofu <dev.bartho@gmail.com>
2023-01-17 17:16:59 +0000

    feat: docker

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,9 @@ const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
 	reactStrictMode: true,
+    output: 'standalone',
 
 	sassOptions: {
         includePaths: [
