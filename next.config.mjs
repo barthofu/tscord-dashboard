5db419ccaafd20c664342e43eb46e974e3ca6aa6
barthofu <dev.bartho@gmail.com>
2024-03-01 09:39:35 +0000

    fix: build errors

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename)
 const nextConfig = {
 
 	reactStrictMode: true,
-    output: standalone,
+    output: 'standalone',
 
 	sassOptions: {
         includePaths: [
